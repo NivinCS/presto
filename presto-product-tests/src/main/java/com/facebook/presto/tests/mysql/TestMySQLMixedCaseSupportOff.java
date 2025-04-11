03ac24faca2d17aa5b7843f116cf473df8981b10
Nivin C S <136801025+NivinCS@users.noreply.github.com>
2025-04-11 19:06:54 +0530

    Mixed case support product tests for mysql connector

--- presto-product-tests/src/main/java/com/facebook/presto/tests/mysql/TestMySQLMixedCaseSupportOff.java
+++ presto-product-tests/src/main/java/com/facebook/presto/tests/mysql/TestMySQLMixedCaseSupportOff.java
@@ -23,64 +23,26 @@ import static io.prestodb.tempto.query.QueryExecutor.query;
 public class TestMySQLMixedCaseSupportOff
         extends ProductTest
 {
-    private static final String SCHEMA_NAME = "mysqlmixedcaseon";
-    private static final String SCHEMA_NAME_UPPER = "MYSQLMIXEDCASEON1";
-
+    private static final String CATALOG = "mysql";
+    /* MySQL connector does not support creating new schemas, so an existing schema is being used instead*/
+    private static final String SCHEMA_NAME = "test";
     private static final String TABLE_NAME = "testtable";
     private static final String TABLE_NAME_0 = "testtable0";
     private static final String TABLE_NAME_MIXED_1 = "TestTable1";
     private static final String TABLE_NAME_UPPER_2 = "TESTTABLE2";
-    private static final String TABLE_NAME_UPPER_SCHEMA_3 = "TESTTABLE3";
-    private static final String TABLE_NAME_UPPER_SCHEMA_4 = "TESTTABLE4";
-    private static final String TABLE_NAME_UPPER_SCHEMA_02 = "testtable02";
 
     /*
-     * Cleanup test case for removing schemas and tables created during mixed-case table tests.
+     * Cleanup test case for removing tables created during mixed-case table tests.
      *
      * This test ensures that all test schemas and tables are properly dropped to maintain a clean test environment.
      */
     @Test
     public void testCleanupMixedCaseTablesAndSchemas()
     {
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME + "." + TABLE_NAME);
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME + "." + TABLE_NAME_0);
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME + "." + TABLE_NAME_MIXED_1);
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2);
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_3);
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_4);
-        query("DROP TABLE IF EXISTS " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02);
-
-        query("DROP SCHEMA IF EXISTS " + SCHEMA_NAME);
-        query("DROP SCHEMA IF EXISTS " + SCHEMA_NAME_UPPER);
-    }
-
-    /*
-     * Test cases for creating schemas with different naming conventions in MySQL.
-     *
-     * This class includes tests for:
-     * 1. Creating a schema with a lowercase name.
-     * 2. Creating a schema with a mixed-case name that has the same syllables as an existing schema.
-     * 3. Creating a schema with a mixed-case name.
-     * 4. Creating a schema with an uppercase name.
-     *
-     * Each test ensures the schema is created successfully.
-     */
-    @Test
-    public void testCreateSchemasWithMixedCaseNames()
-    {
-        String schemaNameMixedSyllables = "MySQLMixedCaseOn";
-        String schemaNameMixed = "MySQLMixedCase";
-
-        query("CREATE SCHEMA IF NOT EXISTS " + SCHEMA_NAME);
-        query("CREATE SCHEMA IF NOT EXISTS " + schemaNameMixedSyllables);
-        query("CREATE SCHEMA IF NOT EXISTS " + schemaNameMixed);
-        query("CREATE SCHEMA IF NOT EXISTS " + SCHEMA_NAME_UPPER);
-
-        assertThat(query("SHOW SCHEMAS"))
-                .contains(row(SCHEMA_NAME.toLowerCase()))
-                .contains(row(schemaNameMixedSyllables.toLowerCase()))
-                .contains(row(schemaNameMixed.toLowerCase()))
-                .contains(row(SCHEMA_NAME_UPPER.toLowerCase()));
+        query("DROP TABLE IF EXISTS " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME);
+        query("DROP TABLE IF EXISTS " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_0);
+        query("DROP TABLE IF EXISTS " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_MIXED_1);
+        query("DROP TABLE IF EXISTS " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2);
     }
 
     /*
@@ -92,68 +54,51 @@ public class TestMySQLMixedCaseSupportOff
     @Test
     public void testCreateTablesWithMixedCaseNames()
     {
-        query("CREATE TABLE " + SCHEMA_NAME + "." + TABLE_NAME_0 + " (name VARCHAR(50), id INT)");
-        query("CREATE TABLE " + SCHEMA_NAME + "." + TABLE_NAME + " (name VARCHAR(50), ID INT)");
-        query("CREATE TABLE " + SCHEMA_NAME + "." + TABLE_NAME_MIXED_1 + " (Name VARCHAR(50), id INT)");
-        query("CREATE TABLE " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " (Name VARCHAR(50), ID INT)");
-        query("CREATE TABLE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_4 + " (Name VARCHAR(50), ID INT)");
-        query("CREATE TABLE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02 + " (name VARCHAR(50), id INT, num DOUBLE)");
-        query("CREATE TABLE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_3 + " (name VARCHAR(50), id INT, num DOUBLE)");
+        query("CREATE TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_0 + " (name VARCHAR(50), id INT)");
+        query("CREATE TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME + " (name VARCHAR(50), ID INT)");
+        query("CREATE TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_MIXED_1 + " (Name VARCHAR(50), id INT)");
+        query("CREATE TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " (Name VARCHAR(50), ID INT)");
 
-        assertThat(query("SHOW TABLES FROM " + SCHEMA_NAME))
+        assertThat(query("SHOW TABLES FROM " + CATALOG + "." + SCHEMA_NAME))
                 .containsOnly(row("testtable0"), row("testtable"), row("testtable1"), row("testtable2"));
 
-        assertThat(query("SHOW TABLES FROM " + SCHEMA_NAME_UPPER))
-                .containsOnly(row("testtable4"), row("testtable02"), row("testtable3"));
     }
 
     /*
-     * This test validates inserting data into tables with different case variations in schema and table names.
+     * This test validates inserting data into tables with different case variations in table names.
      * It ensures that data is inserted and retrieved correctly regardless of case sensitivity.
      */
     @Test
     public void testInsertDataWithMixedCaseNames()
     {
-        query("INSERT INTO " + SCHEMA_NAME + "." + TABLE_NAME + " VALUES ('amy', 112), ('mia', 123)");
-        query("INSERT INTO " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " VALUES ('ann', 112), ('mary', 123)");
-        query("INSERT INTO " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02 + " VALUES ('emma', 112, 200.002), ('mark', 123, 300.003)");
-        query("INSERT INTO " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_3 + " VALUES ('emma', 112, 200.002), ('mark', 123, 300.003)");
+        query("INSERT INTO " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME + " VALUES ('amy', 112), ('mia', 123)");
+        query("INSERT INTO " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " VALUES ('ann', 112), ('mary', 123)");
 
-        assertThat(query("SELECT * FROM " + SCHEMA_NAME + "." + TABLE_NAME))
+        assertThat(query("SELECT * FROM " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME))
                 .containsOnly(row("amy", 112), row("mia", 123));
 
-        assertThat(query("SELECT * FROM " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2))
+        assertThat(query("SELECT * FROM " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2))
                 .containsOnly(row("ann", 112), row("mary", 123));
 
-        assertThat(query("SELECT * FROM " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02))
-                .containsOnly(row("emma", 112, 200.002), row("mark", 123, 300.003));
-
-        assertThat(query("SELECT * FROM " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_3))
-                .containsOnly(row("emma", 112, 200.002), row("mark", 123, 300.003));
     }
 
     /*
-     * This test verifies selecting data from tables with different case variations in schema and table names.
+     * This test verifies selecting data from tables with different case variations in table names.
      * It ensures that queries return correct results regardless of case sensitivity.
      */
     @Test
     public void testSelectDataWithMixedCaseNames()
     {
-        assertThat(query("SELECT * FROM " + SCHEMA_NAME + "." + TABLE_NAME))
+        assertThat(query("SELECT * FROM " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME))
                 .containsOnly(row("amy", 112), row("mia", 123));
 
-        assertThat(query("SELECT name FROM " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " WHERE id = 123"))
+        assertThat(query("SELECT name FROM " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " WHERE id = 123"))
                 .containsOnly(row("mary"));
 
-        assertThat(query("SELECT name FROM " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02 + " WHERE id = 112"))
-                .containsOnly(row("emma"));
-
-        assertThat(query("SELECT name FROM " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_3 + " WHERE id = 123"))
-                .containsOnly(row("mark"));
     }
 
     /*
-     * This test verifies altering tables with different case variations in schema and table names.
+     * This test verifies altering tables with different case variations in table names.
      *
      * Scenarios covered:
      * 1. Adding columns to tables with lowercase, mixed-case, and uppercase names.
@@ -168,29 +113,20 @@ public class TestMySQLMixedCaseSupportOff
     public void testTableAlterWithMixedCaseNames()
     {
         // Add columns to tables with various schema and table name cases
-        query("ALTER TABLE " + SCHEMA_NAME + "." + TABLE_NAME + " ADD COLUMN num REAL");
-        query("ALTER TABLE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02 + " ADD COLUMN num1 REAL");
-        query("ALTER TABLE " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " ADD COLUMN num01 REAL");
-        query("ALTER TABLE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02.toUpperCase() + " ADD COLUMN num2 REAL");
+        query("ALTER TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME + " ADD COLUMN num REAL");
+        query("ALTER TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2 + " ADD COLUMN num01 REAL");
 
         // Verify the added columns
-        assertThat(query("DESCRIBE " + SCHEMA_NAME + "." + TABLE_NAME))
+        assertThat(query("DESCRIBE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME))
                 .contains(row("num", "real", "", ""));
-        assertThat(query("DESCRIBE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02))
-                .contains(row("num1", "real", "", ""));
-        assertThat(query("DESCRIBE " + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2))
+        assertThat(query("DESCRIBE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME_UPPER_2))
                 .contains(row("num01", "real", "", ""));
-        assertThat(query("DESCRIBE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02.toUpperCase()))
-                .contains(row("num2", "real", "", ""));
 
         // Rename columns
-        query("ALTER TABLE " + SCHEMA_NAME + "." + TABLE_NAME + " RENAME COLUMN num TO numb");
-        query("ALTER TABLE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02 + " RENAME COLUMN num1 TO numb01");
+        query("ALTER TABLE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME + " RENAME COLUMN num TO numb");
 
         // Verify column renaming
-        assertThat(query("DESCRIBE " + SCHEMA_NAME + "." + TABLE_NAME))
+        assertThat(query("DESCRIBE " + CATALOG + "." + SCHEMA_NAME + "." + TABLE_NAME))
                 .contains(row("numb", "real", "", ""));
-        assertThat(query("DESCRIBE " + SCHEMA_NAME_UPPER + "." + TABLE_NAME_UPPER_SCHEMA_02))
-                .contains(row("numb01", "real", "", ""));
     }
 }
